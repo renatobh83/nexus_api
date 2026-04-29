0a9bd44bbad18c826158bc019d1b026ecb59cae8
Renato <neo_rlm@hotmail.com>
2026-04-29 15:52:06 -0300

    cors

--- src/api/plugins/fastifyModules.ts
+++ src/api/plugins/fastifyModules.ts
@@ -41,7 +41,7 @@ const fastifyModule = fp(async (fastify: FastifyInstance) => {
     "http://127.0.0.1:8080",
     "http://localhost:8080",
     "undefined",
-    "null",
+    "https://fast.panelapps.site",
     // Adicione o IP da sua VPS se for testar remotamente
   ];
 
