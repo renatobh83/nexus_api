94cca14a2a1945fdbec0c1fae6c3c6620498e7df
Renato <neo_rlm@hotmail.com>
2026-06-05 15:16:34 -0300

    filterCorrecao

--- src/modules/flow/nodes/SendMessage.ts
+++ src/modules/flow/nodes/SendMessage.ts
@@ -5,14 +5,16 @@ const channelService = new ChannelService()
 
 export const SendMessage = {
     async execute(node: any, context: any) {
-        
-        const contato = context.ticket.contato
+
+        const mensagem = node.data.props.find((p: any) => p.k === "Mensagem")?.v;
+        const numero = node.data.props.find((p: any) => p.k === "Numero")?.v;
+        const contato = numero || context.ticket.contato
         const channelId = context.ticket.channelId
-   
-             
+
+
         const channel = await channelService.findChannelOrThrow(channelId)
-        
-        handleSendMessage(channel, contato, context.mensagem || "", null)
+
+        handleSendMessage(channel, contato, mensagem || context.mensagem, null)
 
         return context;
     },
--- src/modules/flow/nodes/TextNode.ts
+++ src/modules/flow/nodes/TextNode.ts
@@ -1,10 +1,6 @@
 export const TextNode = {
   async execute(node: any, context: any) {
-    
     const mensagem = node.data.props.find((p: any) => p.k === "Mensagem")?.v;
-
-    
-
     return {...context, mensagem};
   },
 };
