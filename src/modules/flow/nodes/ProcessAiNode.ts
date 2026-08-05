7401142b66b3c28bb1e349277f49d9459424b6d5
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2026-08-05 08:11:28 -0300

    Update API endpoint and request structure in ProcessAiNode

--- src/modules/flow/nodes/ProcessAiNode.ts
+++ src/modules/flow/nodes/ProcessAiNode.ts
@@ -195,25 +195,18 @@ async function fetchWithRetry(
   tokens = 500,
 ): Promise<Response> {
   const response = await fetch(
-    // "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",/
-  //  "https://generativelanguage.googleapis.com/v1beta/models/gemini-embedding-2:embedContent",
-    `https://generativelanguage.googleapis.com/v1beta/${model}:embedContent?key=${process.env.GEMINI_API_KEY}`,
+    "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
     {
       method: "POST",
       headers: {
-        // Authorization: `Bearer ${process.env.GEMINI_API_KEY}`,
-        //"x-goog-api-key" : String(process.env.GEMINI_API_KEY)!,
+         Authorization: `Bearer ${process.env.GEMINI_API_KEY}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         model: `models/${model}`,
-        content: {
-        parts: [{ text: messages }]
-        }
-       // content: messages,
-        // temperature,
-        // max_tokens: tokens,
-       
+        messages,
+        temperature,
+        max_tokens: tokens,  
       }),
     },
   );
